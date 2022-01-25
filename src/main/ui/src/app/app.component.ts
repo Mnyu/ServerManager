cf76f733e153e1b5a3ec57389782cc8ada6e6f77
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-01-25 22:32:59 +0530

    changing ChangeDetectionStrategy to OnPush from Default

--- src/main/ui/src/app/app.component.ts
+++ src/main/ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ServerService} from "./service/server.service";
 import {BehaviorSubject, catchError, map, Observable, of, startWith} from "rxjs";
 import {AppState} from "./interface/app-state";
@@ -12,7 +12,8 @@ import {NotificationService} from "./service/notification.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
 
